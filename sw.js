4936ca57b71628f861f7c0073a37154fa255a054
drbellschool <drbell@lonestarteacher.com>
2026-09-12 15:40:13 -0500

    Remove service worker so sensitive captures are never cached

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='cup-shell-v1';
-const SHELL=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})));
-});
